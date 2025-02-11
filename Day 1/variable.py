78169d6bc4b3b16fb16a11750a44cba1d63ae588
abin0x <mahmudulabin@gmail.com>
2025-02-11 20:09:16 +0600

    variable add

--- Day 1/variable.py
+++ Day 1/variable.py
@@ -4,3 +4,8 @@ strings='Hello World'
 is_bool=True
 print(age,interset_rate,strings,is_bool)
 print(type(age),type(interset_rate),type(strings),type(is_bool))
+
+# python a varibale type language. It means that you don't have to declare the type of the variable. Python automatically assigns the data type to the variable when you assign a value to the variable.
+
+# like taht,
+# variable_name=value
