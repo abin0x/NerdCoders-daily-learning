c7f1bd3a2be07a3404aa7734a2b9b51a3a164c17
abin0x <mahmudulabin@gmail.com>
2025-02-11 20:23:25 +0600

    Take input

--- Day 1/input.py
+++ Day 1/input.py
@@ -1,2 +1,10 @@
-money=input("Enter the amount of money you have: ")
-print("You have $"+money)
+# money=input("Enter the amount of money you have: ")
+# print("You have $"+money)
+
+first_money=input("Enter the amount of money you have: ")
+second_money=input("Enter the amount of money you have: ")
+# print("You have first $",first_money "and second $",second_money)
+print(f"You have first ${first_money} and second ${second_money}")
+total=int(first_money)+int(second_money)
+
+print(f"Total money you have is ${total}")
