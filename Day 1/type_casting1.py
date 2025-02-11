9db76d2581964f877cf199227d599f6e6eba9f14
abin0x <mahmudulabin@gmail.com>
2025-02-11 20:31:53 +0600

    Type casting

---
+++ Day 1/type_casting1.py
@@ -0,0 +1,28 @@
+# integer থেকে float এ কাস্টিং
+x = 5
+y = float(x)  # x কে float টাইপে কাস্ট
+print(y)  # আউটপুট: 5.0
+
+# float থেকে integer এ কাস্টিং
+x = 5.7
+y = int(x)  # x কে integer টাইপে কাস্ট
+print(y)  # আউটপুট: 5 (decimal পার্ট বাদ যাবে)
+
+# float থেকে string এ কাস্টিং
+x = 5.7
+y = str(x)  # x কে string টাইপে কাস্ট
+print(y)  # আউটপুট: '5.7'
+
+# string থেকে integer এ কাস্টিং
+x = "50"
+y = int(x)  # x কে integer টাইপে কাস্ট
+print(y)  # আউটপুট: 50
+
+# string থেকে float এ কাস্টিং
+x = "50.5"
+y = float(x)  # x কে float টাইপে কাস্ট
+print(y)  # আউটপুট: 50.5
+# integer থেকে string এ কাস্টিং
+x = 50
+y = str(x)  # x কে string টাইপে কাস্ট
+print(y)  # আউটপুট: '50'
