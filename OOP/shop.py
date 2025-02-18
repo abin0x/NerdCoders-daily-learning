74f100290c7c8691e7d22eca4f6e476b501b9192
abin0x <mahmudulabin@gmail.com>
2025-02-18 18:50:54 +0600

    attribute

---
+++ OOP/shop.py
@@ -0,0 +1,15 @@
+class Shop:
+    cart=[]
+
+    def __init__(self,buyer):
+        self.buyer=buyer
+
+    def addToCart(self,item):
+        self.cart.append(item)
+
+
+abinShop=Shop("hasan")
+abinShop.addToCart("tea")
+abinShop.addToCart("watch")
+abinShop.addToCart("t shirt")
+print(abinShop.cart)
