50b2574f81638c0d13b5ef4bceba61d717ab6ded
abin0x <mahmudulabin@gmail.com>
2025-02-20 23:51:55 +0600

    override

---
+++ MORE OOP/override.py
@@ -0,0 +1,27 @@
+class Person:
+    def __init__(self,name,age,hight,weight):
+        self.name = name
+        self.age = age
+        self.hight = hight
+        self.weight = weight
+    def eat(self):
+        print(f"Vat pulao kurma")
+
+    def exercise(self):
+        raise NotImplementedError("Subclass must implement this abstract method")
+
+
+class Circketer(Person):
+    def __init__(self,name,age,hight,weight,runs,wickets):
+        self.runs = runs
+        self.wickets = wickets
+        super().__init__(name,age,hight,weight)
+
+    def eat(self):
+        print(f"Name: {self.name}\nAge: {self.age}\nHight: {self.hight}\nWeight: {self.weight}\nRuns: {self.runs}\nWickets: {self.wickets}")
+    def exercise(self):
+        print(f"{self.name} is doing excercise")
+
+sakib=Circketer("Sakib",35,5.6,75,5000,200)
+sakib.eat()
+sakib.exercise()
--- OOP/shape.py
+++ OOP/shape.py
@@ -3,34 +3,37 @@ class Shape:
         self.name = name
 
 class Circle(Shape):
-    def __init__(self, name):
+    def __init__(self, name, radius):
         super().__init__(name)
+        self.radius = radius
     
-    def area(self, radius):
-        return 3.14 * radius * radius
-class Ractangle(Shape):
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+class Rectangle(Shape):
     def __init__(self, name, length, width):
+        super().__init__(name)
         self.length = length
         self.width = width
-        super().__init__(name)
     
     def area(self):
         return self.length * self.width
 
 class Triangle(Shape):
     def __init__(self, name, base, height):
+        super().__init__(name)
         self.base = base
         self.height = height
-        super().__init__(name)
     
     def area(self):
         return 0.5 * self.base * self.height
 
-circle = Circle("Circle")
-print(circle.area(5))
-ractangle = Ractangle("Ractangle", 5, 10)
-print(ractangle.area())
+# বিভিন্ন শেপের অবজেক্ট তৈরি করা হচ্ছে
+circle = Circle("Circle", 5)
+print(f"Area of {circle.name}: {circle.area()}")
+
+rectangle = Rectangle("Rectangle", 5, 10)
+print(f"Area of {rectangle.name}: {rectangle.area()}")
+
 triangle = Triangle("Triangle", 5, 10)
-
-print(triangle.area())
-
+print(f"Area of {triangle.name}: {triangle.area()}")
